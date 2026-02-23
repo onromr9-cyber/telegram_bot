532e578487b031b1290265e0f6d1dc19485bd9ce
onromr9-cyber <onromr9@gmail.com>
2026-02-23 23:23:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = 5813833511  # senin Telegram ID
 
-ADMIN_ID = 5813833511  # Senin Telegram ID
-
-# Yetki kontrol fonksiyonu
 def is_admin(update: Update):
     return update.effective_user.id == ADMIN_ID
 
@@ -18,14 +16,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not is_admin(update):
         return
-
-    user_text = update.message.text
-    await update.message.reply_text(f"Gelen veri alındı: {user_text}")
+    await update.message.reply_text(f"Gelen veri alındı: {update.message.text}")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-print("Bot çalışıyor...")
-app.run_polling()
+if __name__ == "__main__":
+    print("Bot çalışıyor...")
+    app.run_polling()
